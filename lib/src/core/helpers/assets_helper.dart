e1858bd2039657e1565214680b8a92c7e0ccca76
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2023-10-02 19:58:26 -0300

    flags

--- lib/src/core/helpers/assets_helper.dart
+++ lib/src/core/helpers/assets_helper.dart
@@ -1,5 +1,5 @@
 class AssetsHelpers {
-  static const _assetsFlags = "assets/images/flags/";
+  static const _assetsFlags = "packages/paipfood_package/assets/images/";
   static String flagPtBr = "${_assetsFlags}pt_BR.svg";
   static String flagPt = "${_assetsFlags}pt.svg";
   static String flagUs = "${_assetsFlags}us.svg";
