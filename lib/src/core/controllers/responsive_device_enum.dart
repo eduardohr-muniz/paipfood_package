eaed22fd7cd38c77596803cdf89c928387883a79
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2023-10-04 14:06:45 -0300

    FEAT get_platform

--- lib/src/core/controllers/responsive_device_enum.dart
+++ lib/src/core/controllers/responsive_device_enum.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:universal_io/io.dart';
 
 enum ResponsiveDevice {
   mobile(0),
   tablet(480),
-  desktop(1024);
+  desktop(1024),
+  webPlatform(1024);
 
   final double minWidth;
   const ResponsiveDevice(this.minWidth);
+
+  bool isPlatform(BuildContext context) {
+    final device = _getPlatform();
+    return device == this;
+  }
+
+  bool isDevice(BuildContext context) {
+    final device = _getDevice(context);
+    return device == this;
+  }
 
   ResponsiveDevice _getDevice(BuildContext context) {
     final width = MediaQuery.sizeOf(context).width;
@@ -20,8 +33,14 @@ enum ResponsiveDevice {
     return ResponsiveDevice.mobile;
   }
 
-  bool isDevice(BuildContext context) {
-    final device = _getDevice(context);
-    return device == this;
+  ResponsiveDevice _getPlatform() {
+    if (kIsWeb) {
+      return ResponsiveDevice.webPlatform;
+    } else if (Platform.isAndroid || Platform.isIOS) {
+      return ResponsiveDevice.mobile;
+    } else if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+      return ResponsiveDevice.desktop;
+    }
+    return ResponsiveDevice.webPlatform;
   }
 }
