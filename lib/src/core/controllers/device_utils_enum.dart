65e7b4363a62f7a643722f32f780a2913461c4be
Eduardo Muniz <90477293+eduardohr-muniz@users.noreply.github.com>
2023-10-04 23:40:48 -0300

    refact responsive

--- lib/src/core/controllers/device_utils_enum.dart
+++ lib/src/core/controllers/device_utils_enum.dart
@@ -1,12 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/material.dart';
 import 'package:universal_io/io.dart';
 
+//Enum
 enum DeviceUtils {
   mobile(0),
   tablet(480),
   desktop(1024),
-  webPlatform(1024);
+  webPlatform(-1);
 
   final double minWidth;
   const DeviceUtils(this.minWidth);
@@ -16,7 +17,7 @@ enum DeviceUtils {
     return device == this;
   }
 
-  bool isResponsiveBreakPoint(BuildContext context) {
+  bool _isResponsiveBreakPoint(BuildContext context) {
     final device = _getDevice(context);
     return device == this;
   }
@@ -43,4 +44,86 @@ enum DeviceUtils {
     }
     return DeviceUtils.webPlatform;
   }
+}
+
+//Provider InheritedWidget
+///Add [ResponsiveProvider.builder] in appWidget.
+class ResponsiveProvider extends InheritedWidget {
+  final DeviceUtils device;
+  const ResponsiveProvider({
+    required this.device,
+    required Widget child,
+    super.key,
+  }) : super(child: child);
+
+  @override
+  bool updateShouldNotify(ResponsiveProvider oldWidget) => device != oldWidget.device;
+
+  static Widget builder(BuildContext context, Widget? child) {
+    return _BuildListener(child: child ?? Container());
+  }
+
+  bool get isMobile => device == DeviceUtils.mobile;
+  bool get isTablet => device == DeviceUtils.tablet;
+  bool get isDesktop => device == DeviceUtils.desktop;
+
+  static void _modifyDevice(BuildContext context, void Function(DeviceUtils) deviceCallBack) {
+    final provider = ResponsiveProvider.of(context);
+    DeviceUtils? result;
+    if (DeviceUtils.desktop._isResponsiveBreakPoint(context)) result = DeviceUtils.desktop;
+    if (DeviceUtils.tablet._isResponsiveBreakPoint(context)) result = DeviceUtils.tablet;
+    if (DeviceUtils.mobile._isResponsiveBreakPoint(context)) result = DeviceUtils.mobile;
+    if (provider.device != result) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        deviceCallBack(result!);
+      });
+    }
+  }
+
+  static ResponsiveProvider of(BuildContext context) {
+    final testProvider = context.dependOnInheritedWidgetOfExactType<ResponsiveProvider>();
+    assert(testProvider != null, "Device not exist in BuildContext");
+    return testProvider!;
+  }
+}
+
+//Listenner
+class _BuildListener extends StatefulWidget {
+  final Widget child;
+  const _BuildListener({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_BuildListener> createState() => _BuildListenerState();
+}
+
+class _BuildListenerState extends State<_BuildListener> {
+  DeviceUtils dev = DeviceUtils.desktop;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void modifyDevice(BuildContext context) {
+    ResponsiveProvider._modifyDevice(context, (newDevice) => setState(() => dev = newDevice));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveProvider(
+      device: dev,
+      child: Builder(builder: (context) {
+        modifyDevice(context);
+        return widget.child;
+      }),
+    );
+  }
+}
+
+//extension
+
+extension ResponsiveProviderContext on BuildContext {
+  ResponsiveProvider get responsive => ResponsiveProvider.of(this);
 }
