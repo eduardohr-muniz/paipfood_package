5a8dce4e3d9113525218375c3d684b8a617cfa76
Eduardo Muniz <eduardohr.muniz@outlook.com>
2023-10-24 20:40:55 -0300

    messages

--- lib/src/core/interfaces/messages/messages.dart
+++ lib/src/core/interfaces/messages/messages.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:paipfood_package/src/extensions/context_extension.dart';
 
 import 'i_messages.dart';
 
 class Messages implements IMessages {
   final BuildContext context;
-  Messages._(this.context);
+  Messages({
+    required this.context,
+  });
 
   @override
   void showError(String message) => _showMessage(message, context.color.tertiaryColor, Colors.white);
