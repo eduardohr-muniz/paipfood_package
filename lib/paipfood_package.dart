388b7c6b948084b2208aa89d0e15d7ab40ccd417
Eduardo Muniz <eduardohr.muniz@outlook.com>
2023-10-31 16:06:58 -0300

    exports

--- lib/paipfood_package.dart
+++ lib/paipfood_package.dart
@@ -17,10 +17,16 @@ export 'src/extensions/mask_input_extension.dart';
 export 'src/core/interfaces/messages/toast.dart';
 export 'src/extensions/color_extension.dart';
 export 'src/extensions/theme_extension.dart';
+export 'src/core/exceptions/try_functions.dart';
+export 'src/core/exceptions/exception.dart';
+export 'src/core/provider/toast_provider.dart';
+export 'src/core/interfaces/htpp/htpp_exception.dart';
+export 'src/core/interfaces/htpp/http_dio.dart';
+export 'src/core/interfaces/htpp/http_exception.dart';
+export 'src/core/interfaces/htpp/http_response.dart';
+export 'src/core/interfaces/htpp/i_http.dart';
 export 'src/core/log/log.dart';
 export 'src/core/interfaces/local_storage/local_storage.dart';
-export 'src/core/interfaces/htpp/http_dio.dart';
-export 'src/core/provider/toast_provider.dart';
 //? UI
 export 'src/ui/widgets/buttons/cw_outline_button.dart';
 export 'src/ui/widgets/image/cw_image_get.dart';
@@ -34,5 +40,4 @@ export 'src/ui/widgets/locale/drop_button_locale.dart';
 export 'src/ui/widgets/switchs/cw_switch_light_dark.dart';
 //? State
 //? Exceptions
-export 'src/core/exceptions/try_functions.dart';
 export 'fonts/icomoon_icons.dart';
