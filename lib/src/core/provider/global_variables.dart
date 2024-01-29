ef6fe4eba2f548f92813b415d783725f16d1a753
Eduardo Muniz <eduardohr.muniz@outlook.com>
2024-01-29 15:38:13 -0300

    dependencies

--- lib/src/core/provider/global_variables.dart
+++ lib/src/core/provider/global_variables.dart
@@ -10,18 +10,3 @@ String get uuid => _uuid.v4();
 Map<String, dynamic> mapI18n = {};
 
 const String stringEmpty = " -- ";
-
-int fastHash(String string) {
-  var hash = 0xcbf29ce484222325;
-
-  var i = 0;
-  while (i < string.length) {
-    final codeUnit = string.codeUnitAt(i++);
-    hash ^= codeUnit >> 8;
-    hash *= 0x100000001b3;
-    hash ^= codeUnit & 0xFF;
-    hash *= 0x100000001b3;
-  }
-
-  return hash;
-}
